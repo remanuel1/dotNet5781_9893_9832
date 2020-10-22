3687d3bb70cd13eb34cae97b3e5c40c2044190be
מעין זוהר <mazohar@g.jct.ac.il>
2020-10-22 13:40:11 +0300

    Welcom9832

---
+++ dotNet5781_00_9893_9832/Program9832.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_9893_9832
+{
+    partial class Program
+    {
+        static partial void Welcome9832()
+        {
+            Console.WriteLine("I an also here");
+        }
+    }
+}
