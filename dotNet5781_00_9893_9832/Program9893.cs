4ee5ffc59095ca5b407fa1ab24333f39b61e176f
רננה עמנואל <remanuel@g.jct.ac.il>
2020-10-22 13:16:40 +0300

    initial code + Welcome9893

---
+++ dotNet5781_00_9893_9832/Program9893.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_9893_9832
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9893();
+            Welcome9832();
+            Console.ReadKey();
+
+
+
+        }
+
+        static partial void Welcome9832();
+
+        private static void Welcome9893()
+        {
+            Console.WriteLine("Enter your name: ");
+            string your_name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", your_name);
+        }
+    }
+}
